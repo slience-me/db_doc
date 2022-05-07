2db6bd6ef139d9bc27c9047ddf86fba87af88156
slience_me <slience_me@foxmail.com>
2022-05-07 08:39:18 +0800

    文档

---
+++ src/main/java/xyz/slienceme/db_doc/DbDocApplication.java
@@ -0,0 +1,13 @@
+package xyz.slienceme.db_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/slienceme/db_doc/DbDocApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.slienceme.db_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
